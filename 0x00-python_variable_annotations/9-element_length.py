e2f764fbad13b77cd881660f3d2109bc506a175d
“Liam-YorkRobertson” <liamyorkdeonrobertson@gmail.com>
2023-12-08 03:28:04 +0200

    created a function to reach a desired output

---
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+Complex type-annotated function that a list of tuples, where
+each tuple contains a sequence and its length
+"""
+from typing import Iterable, Sequence, List, Tuple
+
+
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """
+    Returns a list of tuples,
+    where each tuple contains a sequence and its length
+    """
+    return [(i, len(i)) for i in lst]
