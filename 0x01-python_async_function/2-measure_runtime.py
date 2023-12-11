75234b7926d3598702e1acc3644efccd87f6799d
“Liam-YorkRobertson” <liamyorkdeonrobertson@gmail.com>
2023-12-12 01:23:54 +0200

    function that measure the execution time for wait_n

---
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+Function that returns the execution time for wait_n
+"""
+import asyncio
+from time import time
+
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+
+def measure_time(n: int = 0, max_delay: int = 10) -> list[float]:
+    """
+    Returns the execution time for wait_n
+    """
+    start = time()
+    asyncio.run(wait_n(n, max_delay))
+    total = time() - start
+    return (total / n)
