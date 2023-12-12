8433f0ccf9ef8b4ff82ecd2f3d9c2bf25b264d13
“Liam-YorkRobertson” <liamyorkdeonrobertson@gmail.com>
2023-12-12 22:23:09 +0200

    measures total runtime of async_comprehension running 4 times in parelle

---
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+measure_runtime measures time it takes async_comprehension to run four times
+in parellel
+"""
+from typing import List
+import asyncio
+from time import time
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> List[float]:
+    """
+    measures runtime of async_comprehenion running 4 times in parellel
+    """
+    start = time()
+    await asyncio.gather(async_comprehension(), async_comprehension(),
+                         async_comprehension(), async_comprehension())
+    end = time()
+    runtime = end - start
+    return runtime
