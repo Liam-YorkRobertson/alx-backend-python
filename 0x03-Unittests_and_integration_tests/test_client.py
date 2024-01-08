245b434fe549aadc18905b720e792fdf92680ab0
“Liam-YorkRobertson” <liamyorkdeonrobertson@gmail.com>
2024-01-08 13:23:53 +0200

    test for public_repos_url

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -4,7 +4,7 @@ creating tests for the client file
 """
 from parameterized import parameterized
 import unittest
-from unittest.mock import patch, Mock
+from unittest.mock import patch, Mock, PropertyMock
 from typing import Dict
 from client import GithubOrgClient
 
@@ -26,6 +26,14 @@ class TestGithubOrgClient(unittest.TestCase):
         mocked_function.assert_called_once_with(
             "https://api.github.com/orgs/{}".format(org_name)
         )
+
+    @patch("client.GithubOrgClient.org", new_callable=PropertyMock)
+    def test_public_repos_url(self, mocked_property: Mock) -> None:
+        mocked_property.return_value = {'repos_url': "https://api.github."
+                                        "com/users/google/repos"}
+        github_org_client = GithubOrgClient("google")
+        self.assertEqual(github_org_client._public_repos_url,
+                         "https://api.github.com/users/google/repos")
 
 
 if __name__ == '__main__':
